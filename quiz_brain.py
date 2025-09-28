d876261f3a8b0d4b41e49108faae1d433c0b70cb
Michael Popovich <earthmabus@hotmail.com>
2025-09-28 18:32:24 -0400

    updated game to work

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,11 +1,32 @@
+from question_model import Question
+
 import html
+import requests
+
 class QuizBrain:
 
-    def __init__(self, q_list):
+    def __init__(self):
         self.question_number = 0
         self.score = 0
-        self.question_list = q_list
+        self.question_list = None
         self.current_question = None
+
+    def load_questions_from_opentdb(self):
+        parameters = {}
+        parameters['amount'] = 3
+        parameters['type'] = "boolean"
+        response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+        response.raise_for_status()
+
+        # convert question_data into a format that works for our QuizBrain
+        question_bank = []
+        for question in response.json()['results']:
+            question_text = question["question"]
+            question_answer = question["correct_answer"]
+            new_question = Question(question_text, question_answer)
+            question_bank.append(new_question)
+
+        self.question_list = question_bank
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -13,17 +34,14 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text = html.unescape(self.current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer)
+        self.current_question.text = html.unescape(self.current_question.text)
+        return self.current_question
 
     def check_answer(self, user_answer):
+        retval = False
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
-        else:
-            print("That's wrong.")
+            return True
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+        return retval
--- ui.py
+++ ui.py
@@ -1,33 +1,75 @@
 import tkinter
+import question_model
+import quiz_brain
+from tkinter import messagebox
+
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class UI:
-    def __init__(self):
+    def __init__(self, quiz : QuizBrain):
+        self.m_quiz = quiz
+        self.m_currentQuestion = None
         self.m_window = tkinter.Tk()
         self.m_window.title("Quizzler")
         self.m_window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.m_score_label = tkinter.Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.m_score_label = tkinter.Label(text=f"Score: {self.m_quiz.score} / {self.m_quiz.question_number}", fg="white", bg=THEME_COLOR)
         self.m_score_label.grid(row=0, column=1)
+        self.display_score()
 
         self.m_canvas = tkinter.Canvas(width=300, height=250, bg="white")
         self.m_question_text = self.m_canvas.create_text(150, 125, text="[QUESTION HERE]", fill=THEME_COLOR, font=("Arial", 20, "italic"))
         self.m_canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_img = tkinter.PhotoImage(file="images/true.png")
-        self.m_true_button = tkinter.Button(image=true_img, highlightthickness=0, command=self.true_button_clicked)
+        self.m_true_button = tkinter.Button(text="True", highlightthickness=0, command=self.true_button_clicked)
         self.m_true_button.grid(row=2, column=0)
 
         false_img = tkinter.PhotoImage(file="images/false.png")
-        self.m_false_button = tkinter.Button(image=false_img, highlightthickness=0, command=self.false_button_clicked)
+        self.m_false_button = tkinter.Button(text="False", highlightthickness=0, command=self.false_button_clicked)
         self.m_false_button.grid(row=2, column=1)
 
+        # get the next question
+        self.next_question()
+
+    def next_question(self):
+        self.m_currentQuestion = self.m_quiz.next_question()
+        self.m_canvas.itemconfig(self.m_question_text, text=self.m_currentQuestion.text)
+
     def true_button_clicked(self):
-        print("you clicked true")
+        self.check_answer("True")
 
     def false_button_clicked(self):
-        print("you clicked false")
+        self.check_answer("False")
+
+    def check_answer(self, answer):
+        if self.m_quiz.check_answer(answer):
+            messagebox.showinfo("Correct","You guessed the correct answer!")
+        else:
+            correct_answer = "True"
+            if answer.lower() == "true":
+                correct_answer = "False"
+            messagebox.showinfo("Incorrect", f"Sorry, the correct answer was {correct_answer}")
+
+        # update the score and get the next question
+        self.display_score()
+        if self.m_quiz.still_has_questions():
+            self.next_question()
+        else:
+            result = messagebox.askyesno("All Done", f"That was the final question!\nYou scored {self.m_quiz.score} of {self.m_quiz.question_number}\nWould you like to play again?")
+            if result:
+                self.m_currentQuestion = None
+
+                self.m_quiz.question_number = 0
+                self.m_quiz.score = 0
+                self.m_quiz.question_list = None
+                self.m_quiz.current_question = None
+                self.m_quiz.load_questions_from_opentdb()
+
+    def display_score(self):
+        self.m_score_label.config(text=f"Score: {self.m_quiz.score} / {self.m_quiz.question_number}")
 
     def loop(self):
         self.m_window.mainloop()
